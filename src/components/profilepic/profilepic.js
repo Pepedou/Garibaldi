a5fe8e551cd690dd83638b387de5e292c57061ba
Lucía E <Luca E>
2017-02-21 21:20:15 -0600

    Modified artist cover and profile pic

--- src/components/profilepic/profilepic.js
+++ src/components/profilepic/profilepic.js
@@ -3,10 +3,16 @@ import React, {Component} from 'react';
 export default class ProfilePic extends Component {
   render() {
     var profileImage = {
-        backgroundImage: `url(${this.props.image})`
+        background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${this.props.image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center top",
+        backgroundRepeat: "no-repeat"
     }
     return (
-      <div className="col-xs-12 col-sm-6 profileImage" style={profileImage}>
+      <div className="col-xs-12 col-sm-6 ">
+        <div id="profileImage" style={profileImage}>
+            <img src={`${this.props.image}`} alt=""/>
+        </div>
         <div className="artistName">{this.props.name}</div>
       </div>
     );
