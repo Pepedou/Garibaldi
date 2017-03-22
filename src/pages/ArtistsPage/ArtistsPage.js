02eac0f07c9e1a6faf79d405651d6bafe2082c2b
Alan Zambrano <alnzam17@gmail.com>
2017-03-21 19:34:15 -0600

    Included all files needed for using the Squeezebox example in the ArtistsPage

--- src/pages/ArtistsPage/ArtistsPage.js
+++ src/pages/ArtistsPage/ArtistsPage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import SqueezeBox from '../../components/Squeezebox/SqueezeBox';
+import SqueezeBox from '../../components/Squeezebox/Squeezebox';
 
-export default class GalleryPage extends Component {
+export default class ArtistsPage extends Component {
     render() {
+        // <Carousel items={items} active={0} ></Carousel>
+        // const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         return (
             <div className="main-area">
-                <SqueezeBox></SqueezeBox>
+                <SqueezeBox></SqueezeBox>        
             </div>
         );
     }
