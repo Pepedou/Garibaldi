40b6a46433b910f9052fe5cdcaef68698ed53e89
Alan Zambrano Lopez <alan.zambrano@globant.com>
2017-03-24 13:02:29 -0600

    Removed dependency on main.js. Now every jQuery functions starts from the corresponding React component

--- src/components/Squeezebox/SqueezeFrontCard.js
+++ src/components/Squeezebox/SqueezeFrontCard.js
@@ -2,10 +2,14 @@ import React, {Component} from 'react';
 
 export default class SqueezeFrontCard extends Component {
     render() {
+        const artist = this.props.artist;
+        const title = artist === null ? '' : artist.name;
+        const detailCardCss = artist === null ? 'cd-project-content' : 'cd-project-content is-visible';
+
         return (
-            <div className={this.props.styles}>
+            <div className={detailCardCss}>
                 <div>
-                    <h2>{this.props.title}</h2>
+                    <h2>{title}</h2>
                     <em>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, ullam.</em>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus. 
---
+++ src/utils/translateValues.js
@@ -0,0 +1,9 @@
+module.exports = function setTranslateValue(item, translate) {
+	item.css({
+		'-moz-transform': 'translateX(-' + translate + ')',
+		'-webkit-transform': 'translateX(-' + translate + ')',
+		'-ms-transform': 'translateX(-' + translate + ')',
+		'-o-transform': 'translateX(-' + translate + ')',
+		'transform': 'translateX(-' + translate + ')',
+	});
+}
