ff867bc2c1d1da171b3056aa72696da5945b62e8
Lucía E <Luca E>
2017-05-23 18:29:28 -0500

    Tests deleted

--- src/components/ArtistCover/artistcover.test.js
+++
@@ -1,8 +0,0 @@
-import ArtistCover from './artistcover';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-test('ArtistCover renders', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ArtistCover />, div);
-});
