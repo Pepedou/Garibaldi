cc239ef2b7838c18eee8cc70f5d2190ebb0aa774
Lucía E <Luca E>
2017-05-23 18:52:11 -0500

    Case sensitive changes

--- src/components/extendedcard/extendedcard.test.js
+++
@@ -1,8 +0,0 @@
-import ExtendedCard from './extendedcard';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-test('ExtendedCard renders', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ExtendedCard detail={{name: 'Something'}} />, div);
-});
