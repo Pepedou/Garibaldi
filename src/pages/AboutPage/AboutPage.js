81db52b844f3e87919672007c081a0ea3a7ba6e1
lucechal <lucechal@gmail.com>
2017-11-17 19:34:11 -0600

    more changes

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -14,15 +14,12 @@ class AboutPage extends Component {
                     <div className="col-xs-12 col-md-8 col-lg-6">
                         <center>
                             <img src={images.gray_logo} id="registerLogo" alt=""/>
-                            <div className="title">El archivero digital de arte para creadores. </div>
+                            <div className="title">Archivero digital de arte para creadores. </div>
                             <p>
                                 Surge de la observación de la necesidad generalizada por ordenar y poder mantener el control de la producción artística de manera práctica y eficiente. 
                             </p>
                             <p>
                                 Es una herramienta fundamental pensada no solo para artistas, fotógrafos, arquitectos y diseñadores sino también para coleccionistas, galerías, gestores y centros culturales.
-                            </p>
-                            <p>
-                                Bienvenidxs a Artchive. Esperamos que sea la herramienta de control interno y externo de cualquier colección de arte por antonomasia.
                             </p>
                         </center>
                     </div>
