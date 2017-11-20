38ec719b6a7e1566aaef8baa52115729c23b1d62
Alan Zambrano <alnzam17@gmail.com>
2017-11-20 11:17:09 -0600

    GAR-60: Added ExportCategories reset action

--- src/redux/reducers/exportCategories/actions.js
+++ src/redux/reducers/exportCategories/actions.js
@@ -4,3 +4,7 @@ export const loadExportCategories = (artPieces) => {
     //TODO: Change to Thunk and call service.loadTemplates
     return { type: types.EXPORT_CATEGORIES_LOAD_CATEGORIES, payload: artPieces }
 }
+
+export const resetExportCategories = () => {
+    return { type: types.EXPORT_CATEGORIES_RESET }
+}
